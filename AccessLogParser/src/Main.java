7add42ba7975a7f58f4f6a1f10f622ddda80745c
Alex <ALagutenko@inno.tech>
2022-11-21 14:55:13 +0300

    fix project

--- AccessLogParser/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-    System.out.println("Введите текст и нажмите <Enter>: " +
-            Math.random());
-    String text = new Scaner(System.in).nextLine();
-    System.out.println("Длина текста: "+ text.lengh());
-    }
-}
