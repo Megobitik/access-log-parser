88654b2c48159787d4d3d1ead12cbd258323aa54
Alex <ALagutenko@inno.tech>
2022-11-21 14:50:56 +0300

    update main

--- AccessLogParser/src/Main.java
+++ AccessLogParser/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
-    System.out.println("Случайное число от 0 до 1: " +
+    System.out.println("Введите текст и нажмите <Enter>: " +
             Math.random());
+    String text = new Scaner(System.in).nextLine();
+    System.out.println("Длина текста: "+ text.lengh());
     }
 }
